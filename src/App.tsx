954ef5acbf08a49ce480c4b7b4092e6165b129c8
Wigor <wigor21@hotmail.com>
2025-01-23 17:47:06 -0300

    feat: add theme with styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <RouterProvider router={router} />
-    </>
+    </ThemeProvider>
   );
 };
 
