556409c493e2cdc7c3d7e48a61711638a09acc01
Wigor <wigor21@hotmail.com>
2025-01-23 17:54:44 -0300

    add: globals and mixins

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
+import { GlobalStyle } from "styles/globals";
 
 const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <RouterProvider router={router} />
     </ThemeProvider>
   );
---
+++ src/styles/globals.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from "styled-components";
+import { mixins } from "./mixins";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: ${(props) => props.theme.colors.background};
+    color: ${(props) => props.theme.colors["base-text"]};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    ${mixins.fonts.textM};
+  }
+
+  button {
+    border: none;
+    background: transparent;
+    cursor: pointer;
+  }
+  
+  img {
+    max-width: 100%;
+    display: block;
+  }
+`;
